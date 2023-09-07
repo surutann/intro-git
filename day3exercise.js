0a5d6ddce5081d2deb7a954ec62b19117c172b15
surutann <sulthanm55@gmail.com>
2023-09-07 11:00:03 +0700

    add new file day3exercise.js

---
+++ day3exercise.js
@@ -0,0 +1,40 @@
+// Exercise 1. Write a code to display the multiplication table of a given integer.
+// Input
+let number = 9;
+let resultNumber = "";
+//Process
+for(let i = 1; i <= 10; i++){
+    let result = number * i;
+    resultNumber += `${number} x ${i} = ${result} \n`;
+};
+//Output
+console.log(resultNumber);
+
+
+// Exercise 2. Write a code to check whether a string is a palindrome or not.
+
+
+// Exercise 3. Write a code to convert centimeter to kilometer.
+// Input
+let centi = 100000;
+//Process
+let km = centi / 100000;
+//Output
+console.log(`${centi} cm = ${km} km`);
+
+
+// Exercise 4. Write a code to format number as currency (IDR)
+// Input
+
+// Process
+
+
+
+// Exercise 5.
+// Exercise 6.
+// Exercise 7.
+// Exercise 8.
+// Exercise 9.
+// Exercise 10.
+// Exercise 11.
+// Exercise 12.
